27b19930b993bf00c3db901d7df27fa980778429
Iris Tsai <mayday6724@gmail.com>
2023-12-16 23:12:50 +0800

    quotes & carousel plugin, img upload

--- js/animation.js
+++ js/animation.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     if (scrollPosition > document.querySelector(".banner").offsetHeight) {
       $("#nav").css(
         "cssText",
-        "background-color: rgba(2, 2, 0, 0.8) !important"
+        "background-color: rgba(62, 51, 47, 0.8) !important"
       );
       $(".navbar-brand").css("cssText", "color: #fffefb");
     } else {
@@ -26,6 +26,26 @@ $(document).ready(function () {
 
         return opacity;
       },
+    });
+  });
+
+  var docWidth = $("body").width(),
+    $wrap = $("#wrap"),
+    $images = $("#wrap .hb"),
+    slidesWidth = $wrap.width();
+
+  $(window).on("resize", function () {
+    docWidth = $("body").width();
+    slidesWidth = $wrap.width();
+  });
+
+  $(document).mousemove(function (e) {
+    var mouseX = e.pageX,
+      offset = (mouseX / docWidth) * slidesWidth - mouseX / 2;
+
+    $images.css({
+      "-webkit-transform": "translate3d(" + -offset + "px,0,0)",
+      transform: "translate3d(" + -offset + "px,0,0)",
     });
   });
 });
