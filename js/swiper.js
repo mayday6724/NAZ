74950bf0c06dfd0827af78d61353e787a7546ce9
Iris Tsai <mayday6724@gmail.com>
2024-04-19 12:13:25 +0800

    swiper js initialize change

--- js/swiper.js
+++ js/swiper.js
@@ -1,21 +1,42 @@
 // JavaScript Document
 
 $(document).ready(function () {
-  var swiper = new Swiper(".mySwiper", {
-    speed: 2000, //2sec
-    spaceBetween: 0, //space between pictures
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500, //duration of a picutre display
-      disableOnInteraction: false,
-    },
+  // var swiper = new Swiper(".mySwiper", {
+  //   speed: 2000, //2sec
+  //   spaceBetween: 0, //space between pictures
+  //   centeredSlides: true,
+  //   autoplay: {
+  //     delay: 2500, //duration of a picutre display
+  //     disableOnInteraction: false,
+  //   },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+  // });
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "vertical",
+    loop: true,
+
+    // If we need pagination
     pagination: {
       el: ".swiper-pagination",
-      clickable: true,
     },
+
+    // Navigation arrows
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
     },
   });
 });
