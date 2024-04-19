31c15c279a886b82f21052f28c78396d654638ae
Iris Tsai <mayday6724@gmail.com>
2024-04-19 19:45:58 +0800

    update swiper in js

--- js/swiper.js
+++ js/swiper.js
@@ -1,23 +1,6 @@
 // JavaScript Document
 
 $(document).ready(function () {
-  // var swiper = new Swiper(".mySwiper", {
-  //   speed: 2000, //2sec
-  //   spaceBetween: 0, //space between pictures
-  //   centeredSlides: true,
-  //   autoplay: {
-  //     delay: 2500, //duration of a picutre display
-  //     disableOnInteraction: false,
-  //   },
-  //   pagination: {
-  //     el: ".swiper-pagination",
-  //     clickable: true,
-  //   },
-  //   navigation: {
-  //     nextEl: ".swiper-button-next",
-  //     prevEl: ".swiper-button-prev",
-  //   },
-  // });
   const swiper = new Swiper(".swiper", {
     // Optional parameters
     direction: "vertical",
